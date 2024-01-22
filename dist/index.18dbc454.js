b8e495b6923086395cbe7357765ac631eb25604b
Emma Ådahl Görling <143135641+EmmaGorling@users.noreply.github.com>
2024-01-22 12:08:07 +0100

    Har använt Malins kod för menyn

--- dist/index.18dbc454.js
+++ dist/index.18dbc454.js
@@ -579,6 +579,22 @@ function hmrAccept(bundle /*: ParcelRequire */ , id /*: string */ ) {
 }
 
 },{}],"1SICI":[function(require,module,exports) {
+"use strict";
+// Hämta in meny-knapparna
+let openBtn = document.getElementById("open-menu");
+let closeBtn = document.getElementById("close-menu");
+//eventlyssnare
+openBtn.addEventListener("click", toggleMenu);
+closeBtn.addEventListener("click", toggleMenu);
+//Toggla fram navigeringsmenyn
+function toggleMenu() {
+    let navMenuEl = document.getElementById("nav-menu");
+    //hämtar in css för menyn
+    let style = window.getComputedStyle(navMenuEl);
+    //koll om navigering är synlig eller ej, ändrar display block/none
+    if (style.display === "none") navMenuEl.style.display = "block";
+    else navMenuEl.style.display = "none";
+}
 
 },{}]},["iqNlW","1SICI"], "1SICI", "parcelRequire53e8")
 
